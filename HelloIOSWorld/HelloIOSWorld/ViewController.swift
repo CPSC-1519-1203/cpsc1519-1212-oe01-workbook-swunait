d4e85ff93e457270532686a0bef1a9f955fe3434
Sam Wu <swu@nait.ca>
2022-01-10 17:47:24 -0800

    End of Jan 10 class

---
+++ HelloIOSWorld/HelloIOSWorld/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HelloIOSWorld
+//
+//  Created by user211011 on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Custom Outlets
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    // MARK: View Events
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: Custom Actions
+    @IBAction func changeGreeting(_ sender: UIButton) {
+        
+    }
+    
+}
+
