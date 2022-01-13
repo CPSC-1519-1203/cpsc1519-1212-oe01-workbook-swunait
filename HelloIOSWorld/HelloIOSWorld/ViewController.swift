ed6d73dc3175eed85a4cd47ac5e064d3be974ca4
Sam Wu <swu@nait.ca>
2022-01-12 17:06:33 -0800

    End of Jan 12 demo

--- HelloIOSWorld/HelloIOSWorld/ViewController.swift
+++ HelloIOSWorld/HelloIOSWorld/ViewController.swift
@@ -24,8 +24,25 @@ class ViewController: UIViewController {
 
     // MARK: Custom Actions
     @IBAction func changeGreeting(_ sender: UIButton) {
+        if let name = nameTextField.text {
+            if let age = ageTextField.text {
+                let message = "Hello \(name). You are \(age) years old"
+                resultLabel.text = message
+            }
+        }
         
     }
     
+    @IBAction func dismissKeyboard(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        
+    }
+    
+    @IBAction func dismissKeypad(_ sender: Any) {
+        ageTextField.resignFirstResponder()
+        nameTextField.resignFirstResponder()
+    }
+    
 }
+
 
