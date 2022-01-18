ed1afbcf705921a88fe25daf96f6f585ff63dee2
Sam Wu <swu@nait.ca>
2022-01-17 20:54:44 -0700

    End of Jan 17 class

---
+++ Chapter08Demo/Chapter08Demo/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Chapter08Demo
+//
+//  Created by Sam WU on 2022-01-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    func convertToFahrenheit(forCelisusValue celisus: Double) -> Double {
+        var fahrenheitValue = 0.00
+        fahrenheitValue = (celisus * 9/5) + 32
+        return fahrenheitValue
+    }
+    
+    var currentCelsiusValue : Double = 0.00
+    
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    
+    @IBOutlet weak var celisusTextField: UITextField!
+    
+    @IBOutlet weak var celsisuStepper: UIStepper!
+    
+    @IBOutlet weak var celsiusSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        celisusTextField.text = "\(currentCelsiusValue)"
+    }
+
+    @IBAction func convertButtonTapped(_ sender: UIButton) {
+        currentCelsiusValue = Double(celisusTextField.text!) ?? 0
+        let fahrenheitValue = convertToFahrenheit(forCelisusValue: currentCelsiusValue)
+        fahrenheitLabel.text = "\(fahrenheitValue)℉"
+        celsisuStepper.value  = currentCelsiusValue
+        celsiusSlider.value = Float(currentCelsiusValue)
+    }
+    
+    @IBAction func celisusStepperChanged(_ sender: UIStepper) {
+        currentCelsiusValue = celsisuStepper.value
+        let fahrenheitValue = convertToFahrenheit(forCelisusValue: currentCelsiusValue)
+        fahrenheitLabel.text = "\(fahrenheitValue)℉"
+        celisusTextField.text = "\(currentCelsiusValue)"
+        celsiusSlider.value = Float(currentCelsiusValue)
+    }
+    
+    @IBAction func celisusSliderChanged(_ sender: UISlider) {
+        currentCelsiusValue = Double(celsiusSlider.value)
+        let fahrenheitValue = convertToFahrenheit(forCelisusValue: currentCelsiusValue)
+        fahrenheitLabel.text = "\(fahrenheitValue)℉"
+        celisusTextField.text = "\(currentCelsiusValue)"
+        celsisuStepper.value = currentCelsiusValue
+       
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        celisusTextField.resignFirstResponder()
+    }
+    
+    
+}
+
