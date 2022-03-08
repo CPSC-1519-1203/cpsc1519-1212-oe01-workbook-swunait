eee6ca436e63a909239ba8fedcd53f404cbf3718
Sam Wu <swu@nait.ca>
2022-03-07 18:26:18 -0700

    Added TableView demo projects

--- Chapter18Demo/Chapter18Demo/ViewController.swift
+++ Chapter18Demo/Chapter18Demo/ViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return .portrait
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let value = UIInterfaceOrientation.portrait.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
     }
 
 
